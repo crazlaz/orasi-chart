7e9149a6d7427d69d0c103de545f93f4e72a93f1
daGodz <uribae@protonmail.com>
2022-10-11 12:41:37 -0400

    chart

---
+++ src/WFdata.js
@@ -0,0 +1,230 @@
+export const UserData = [
+    {
+      name: 'ss-minio-ui',
+      entityId: 'HTTP_CHECK-56584A0AB8EC9B7A',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-ct',
+      entityId: 'HTTP_CHECK-6184A24985190817',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-ams',
+      entityId: 'HTTP_CHECK-F021B9363E01AD21',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-bi',
+      entityId: 'HTTP_CHECK-3070F296E0BDC085',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-workspace',
+      entityId: 'HTTP_CHECK-C7A2DC4E9E92FB66',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-minio',
+      entityId: 'HTTP_CHECK-31F6A9B3A00FA84C',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-bi',
+      entityId: 'HTTP_CHECK-F4FC944E33A805A1',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-tds',
+      entityId: 'HTTP_CHECK-6FA07F3DB3F4C72A',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'mesos-master',
+      entityId: 'HTTP_CHECK-32F38A7440AAF8F1',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-minio-api',
+      entityId: 'HTTP_CHECK-F1F9648D1293ED84',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-ocr',
+      entityId: 'HTTP_CHECK-258C99EF2DA5D02A',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-marathon',
+      entityId: 'HTTP_CHECK-224E7DA4E2A254C8',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-kibana',
+      entityId: 'HTTP_CHECK-DDBA14C37EF3F060',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-mesos-agent',
+      entityId: 'HTTP_CHECK-8C6E45917B3BB2AA',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-keycloack',
+      entityId: 'HTTP_CHECK-EBF42AB49D783ADA',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-botmanager',
+      entityId: 'HTTP_CHECK-42FF8B82F131A09D',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'mesos-agent2',
+      entityId: 'HTTP_CHECK-90D07AB0355C235D',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-workspace',
+      entityId: 'HTTP_CHECK-4870A0E2AE694C15',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-mesos-master',
+      entityId: 'HTTP_CHECK-5B23D6BF5E5B9D9B',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'mesos-agent',
+      entityId: 'HTTP_CHECK-78AD4A05F7BA51EB',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-nexus',
+      entityId: 'HTTP_CHECK-D94EDF63195DF07C',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-mesos',
+      entityId: 'HTTP_CHECK-081E0E2E8DF6640E',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-ocr',
+      entityId: 'HTTP_CHECK-D430991A79464A81',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-amms',
+      entityId: 'HTTP_CHECK-C13F250FCD785A74',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-rabbitmq',
+      entityId: 'HTTP_CHECK-02ECCD6EFA39B3A5',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-sqc',
+      entityId: 'HTTP_CHECK-1FB6D404BA406A1F',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-ags',
+      entityId: 'HTTP_CHECK-AAC71C86386708F0',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-ss-http-monitor',
+      entityId: 'HTTP_CHECK-0DF1C9AF5C82224C',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-wms',
+      entityId: 'HTTP_CHECK-068FCB7AD3ECBE52',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'kibana',
+      entityId: 'HTTP_CHECK-8A6A82611D05F2A9',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-rabbitmq',
+      entityId: 'HTTP_CHECK-CBDD31F7F5CAB4CE',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-marathon',
+      entityId: 'HTTP_CHECK-D87EF6740020B897',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-nexus',
+      entityId: 'HTTP_CHECK-17A1B65652C1E252',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'prod-polaris-vault',
+      entityId: 'HTTP_CHECK-6926C2C150E50FC9',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-bot-manager',
+      entityId: 'HTTP_CHECK-9E17243D7B978932',
+      type: 'HTTP',
+      enabled: false
+    },
+    {
+      name: 'prod-polaris-ct',
+      entityId: 'HTTP_CHECK-4978A7D96A84B4DA',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-kibana',
+      entityId: 'HTTP_CHECK-1B1AE2A8A0AF16F8',
+      type: 'HTTP',
+      enabled: true
+    },
+    {
+      name: 'ss-vault',
+      entityId: 'HTTP_CHECK-EA0BE7BDEAD08A52',
+      type: 'HTTP',
+      enabled: true
+    }
+  ]
